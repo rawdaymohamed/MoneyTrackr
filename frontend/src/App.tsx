69dd49c0cf23638c198091c3507d29d2926d03a5
= <rawdaymohamed@gmail.com>
2025-07-07 12:22:25 +0300

    Chore: Install 3rd party libs and configure react router dom routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,38 @@
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Home from './pages/Dashboard/Home'
+import Signup from './pages/Auth/Signup'
+import Income from './pages/Dashboard/Income'
+import Expenses from './pages/Dashboard/Expenses'
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-3xl font-bold">Welcome to MoneyTrackr</h1>
-      <p>Your personal finance tracking app.</p>
-      <p>Track your expenses, income, and savings effortlessly.</p>
-      <p>Under development</p>
-      <p>Stay tuned for more features!</p>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+
+        </Routes>
+      </Router>
     </div>
   )
 }
 
 export default App
+const Root = () => {
+  // chec if user is authenticated
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  // Redirect to login if not authenticated
+  return (isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />);
+};
